b60feef35e6e08fe1cab4db14c16bad74d2c3b7b
Gringo <nogringo@yandex.com>
2026-01-05 11:31:01 +0100

    feat: add relays

--- lib/services/nostr_mail_service.dart
+++ lib/services/nostr_mail_service.dart
@@ -37,6 +37,11 @@ class NostrMailService extends GetxService {
           'wss://relay.damus.io',
           'wss://nos.lol',
           'wss://relay.nostr.band',
+          'wss://relay.primal.net',
+          'wss://relay.coinos.io',
+          'wss://nostr-01.uid.ovh',
+          'wss://nostr-02.uid.ovh',
+          'wss://nostr-01.yakihonne.com',
         ],
       ),
     );
