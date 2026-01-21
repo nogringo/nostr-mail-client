a86ae74964d77fc77c666b703074d3b42fef4d5f
Gringo <nogringo@yandex.com>
2026-01-21 11:02:09 +0100

    fix: database in application support

--- android/app/src/main/kotlin/com/example/client/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/client/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.client
+package app.nostrmail.client
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/services/storage_service_io.dart
+++ lib/services/storage_service_io.dart
@@ -1,14 +1,11 @@
-import 'dart:io';
-
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
 
 Future<Database> openDatabaseIo() async {
-  final dir = await getApplicationDocumentsDirectory();
-  final dbDir = Directory('${dir.path}/nostr_mail');
-  if (!await dbDir.exists()) {
-    await dbDir.create(recursive: true);
-  }
-  final dbPath = '${dbDir.path}/nostr_mail.db';
+  final dir = await getApplicationSupportDirectory();
+  final dbName = kDebugMode ? 'nostr_mail_dev.db' : 'nostr_mail.db';
+  final dbPath = p.join(dir.path, dbName);
   return databaseFactoryIo.openDatabase(dbPath);
 }
