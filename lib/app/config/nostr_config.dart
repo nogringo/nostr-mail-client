ab7704963095238210ec07ad912a76e3ed1b11bd
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-07 13:42:01 +0200

    chore: update recommended Nostr endpoints

--- lib/app/config/nostr_config.dart
+++ lib/app/config/nostr_config.dart
@@ -1,52 +1,38 @@
 class NostrConfig {
   static const bootstrapRelays = [
-    'wss://relay.damus.io',
-    'wss://nos.lol',
-    'wss://relay.primal.net',
-    'wss://relay.coinos.io',
     'wss://relay.nmail.li',
-    'wss://nostr-01.uid.ovh',
-    'wss://nostr-02.uid.ovh',
     'wss://nostr-01.yakihonne.com',
+    'wss://nos.lol',
+    'wss://relay.damus.io',
+    'wss://relay.primal.net',
   ];
 
   /// Popular relays used to broadcast signaling events (kinds 0, 10002,
   /// 10050, 10063) widely for maximum discoverability.
   static const popularRelays = [
-    'wss://relay.damus.io',
-    'wss://nos.lol',
-    'wss://relay.primal.net',
-    'wss://relay.coinos.io',
     'wss://relay.nmail.li',
-    'wss://nostr-01.uid.ovh',
-    'wss://nostr-02.uid.ovh',
-    'wss://relay.camelus.app',
     'wss://nostr-01.yakihonne.com',
-    'wss://nostr-02.yakihonne.com',
+    'wss://nos.lol',
     'wss://purplepag.es',
+    'wss://relay.damus.io',
+    'wss://relay.primal.net',
   ];
 
   static const recommendedInboxOutboxRelays = [
-    'wss://relay.damus.io',
-    'wss://relay.camelus.app',
     'wss://relay.nmail.li',
-    'wss://nostr-01.uid.ovh',
-    'wss://nostr-02.uid.ovh',
+    'wss://nostr-01.yakihonne.com',
+    'wss://relay.damus.io',
     'wss://relay.primal.net',
   ];
 
   static const recommendedDmRelays = [
     'wss://auth.nostr1.com',
     'wss://relay.nmail.li',
-    'wss://nostr-01.uid.ovh',
-    'wss://nostr-02.uid.ovh',
   ];
 
   static const recommendedBlossomServers = [
-    'https://blossom.yakihonne.com',
     'https://blossom.nmail.li',
-    'https://blossom-01.uid.ovh',
-    'https://blossom-02.uid.ovh',
+    'https://blossom.yakihonne.com',
     'https://blossom.primal.net',
   ];
 
