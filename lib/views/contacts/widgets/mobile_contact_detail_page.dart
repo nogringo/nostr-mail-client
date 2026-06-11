5537c23cda2569167e645ada16332c4a63975fb9
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-11 18:48:13 +0200

    fix: stop showing the contact name twice on mobile

--- lib/views/contacts/widgets/mobile_contact_detail_page.dart
+++ lib/views/contacts/widgets/mobile_contact_detail_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../controllers/contacts_controller.dart';
-import '../../../l10n/generated/app_localizations.dart';
 import 'contact_actions.dart';
 import 'contact_detail_pane.dart';
 
@@ -14,14 +13,12 @@ class MobileContactDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<ContactsController>();
-    final l = AppLocalizations.of(context);
     return Obx(() {
       final contact = controller.addressBookService.contacts.firstWhereOrNull(
         (contact) => contact.uid == uid,
       );
       return Scaffold(
         appBar: AppBar(
-          title: Text(contact?.index.formattedName ?? l.contactsTitle),
           actionsPadding: const EdgeInsets.only(right: 8),
           actions: [
             if (contact != null)
